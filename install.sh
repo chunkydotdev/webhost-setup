3aa36fda91592c956ea2cfafd6df9de96a70c593
Magnus Junghard Huttu <magnushuttu@gmail.com>
2022-08-31 13:23:03 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Install nginx webhost and a domain on debian 
+
 echo "downloading webhost repo..."
 git pull origin https://github.com/Storken/webhost-setup/ -o .webhost-setup
 cd .webhost-setup/debian-11
