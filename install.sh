ee40385af28a719f060060adf469f09c9730c58f
Magnus Junghard Huttu <magnushuttu@gmail.com>
2022-08-31 13:36:58 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Install nginx webhost and a domain on debian 
 
+echo "updating apt"
+sudo apt update
+sudo apt install svn -y
+
 echo "downloading webhost repo..."
 svn checkout https://github.com/Storken/webhost/trunk/debian-11
 cd debian-11
