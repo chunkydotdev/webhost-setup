595a7a13e003f6327fcc829a4e861a154fbe50a0
Magnus Junghard Huttu <magnushuttu@gmail.com>
2022-08-31 17:59:24 +0200

    update variables

--- install.sh
+++ install.sh
@@ -10,4 +10,6 @@ svn export https://github.com/Storken/webhost-setup.git/trunk/debian-11
 cd debian-11
 echo "done"
 echo "starting make script"
-make
+make install_nginx setup_firewall 
+read -p "What domain do you want to use (without http/https): " DOMAIN
+make setup_website setup_cert domain=$DOMAIN
