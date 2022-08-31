6c46aceba3ad084242628784b402bd72c876e0a1
Magnus Junghard Huttu <magnushuttu@gmail.com>
2022-08-31 13:21:16 +0200

    update install script

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-git pull origin https://github.com/Storken/webhost-setup/
+echo "downloading webhost repo..."
+git pull origin https://github.com/Storken/webhost-setup/ -o .webhost-setup
+cd .webhost-setup/debian-11
+echo "done"
+echo "starting make script"
+make
