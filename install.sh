28a591a88298c2044e91f1141559156dd4d1689b
Magnus Junghard <magnushuttu@gmail.com>
2022-10-14 12:31:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo "updating apt"
 sudo apt update
 sudo apt upgrade -y
 sudo apt install subversion make ufw nginx -y
-
+sudo groupadd developers
+	
 echo "packages are updated"
 read -p "do you want to set up a dev user? (y/n) " 
 if [[ $REPLY -eq "y" ]]
@@ -13,7 +14,21 @@ then
 	adduser dev
 	usermod -aG sudo dev
 	getent group sudo
+	gpasswd -a dev developers
 	cp -rf ~/.ssh /home/dev
-	chown -R dev:dev /home/dev/.ssh
+	sudo chown -R dev:dev /home/dev/.ssh
+	sudo chown -R dev:developers /var/www/
+	sudo chmod -R g+w /var/www/
+fi
+
+read -p "do you want to set up a git ci user? (y/n) " 
+if [[ $REPLY -eq "y" ]]
+then 
+	adduser gitci
+	gpasswd -a gitci developers
+	cp -rf ~/.ssh /home/dev
+	sudo chown -R gitci:developers /home/dev/.ssh	
+	sudo chown -R gitci:developers /var/www/
+	sudo chmod -R g+w /var/www/
 fi
 echo "done"
